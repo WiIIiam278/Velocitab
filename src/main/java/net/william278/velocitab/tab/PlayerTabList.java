d73530c903ad21ea3e2545eee41d5019f4b793bc
William <will27528@gmail.com>
2023-02-19 12:47:58 +0000

    Reverse role comparable sort order

--- src/main/java/net/william278/velocitab/tab/PlayerTabList.java
+++ src/main/java/net/william278/velocitab/tab/PlayerTabList.java
@@ -14,7 +14,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
 public class PlayerTabList {
     private final Velocitab plugin;

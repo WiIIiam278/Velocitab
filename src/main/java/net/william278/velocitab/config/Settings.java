0abcb3d2b3e567148b6148fc88a10207e379058c
William <will27528@gmail.com>
2023-04-20 23:51:19 +0100

    Update config file header

--- src/main/java/net/william278/velocitab/config/Settings.java
+++ src/main/java/net/william278/velocitab/config/Settings.java
@@ -36,7 +36,8 @@ import java.util.Map;
         ┃       Velocitab Config       ┃
         ┃    Developed by William278   ┃
         ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
-        ┗╸ Placeholders: %players_online%, %max_players_online%, %local_players_online%, %current_date%, %current_time%, %username%, %server%, %ping%, %prefix%, %suffix%, %role%""")
+        ┣╸ Information: https://william278.net/project/velocitab
+        ┗╸ Documentation: https://william278.net/docs/velocitab""")
 public class Settings {
 
     @Getter

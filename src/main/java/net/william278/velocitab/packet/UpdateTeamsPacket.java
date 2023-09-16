8486424874a7b052641c910072ca75c8617220e3
AlexDev_ <alessandrodalfovo2003@gmail.com>
2023-09-16 11:18:44 +0200

    Re-Added final keyword on plugin parameter in UpdateTeamsPacket

--- src/main/java/net/william278/velocitab/packet/UpdateTeamsPacket.java
+++ src/main/java/net/william278/velocitab/packet/UpdateTeamsPacket.java
@@ -43,7 +43,7 @@ import java.util.stream.Collectors;
 @Accessors(fluent = true)
 public class UpdateTeamsPacket implements MinecraftPacket {
 
-    private Velocitab plugin;
+    private final Velocitab plugin;
 
     private String teamName;
     private UpdateMode mode;

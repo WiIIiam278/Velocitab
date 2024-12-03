5a4605311752419795b029487e456c7b705831f5
AlexDev_ <56083016+alexdev03@users.noreply.github.com>
2024-12-03 23:14:00 +0100

    Added 1.21.4 to workflows Added 1.21.4 to Protocol765Adapter

--- src/main/java/net/william278/velocitab/packet/Protocol765Adapter.java
+++ src/main/java/net/william278/velocitab/packet/Protocol765Adapter.java
@@ -41,7 +41,8 @@ public class Protocol765Adapter extends Protocol404Adapter {
                 ProtocolVersion.MINECRAFT_1_20_3,
                 ProtocolVersion.MINECRAFT_1_20_5,
                 ProtocolVersion.MINECRAFT_1_21,
-                ProtocolVersion.MINECRAFT_1_21_2
+                ProtocolVersion.MINECRAFT_1_21_2,
+                ProtocolVersion.MINECRAFT_1_21_4
         ));
     }
 

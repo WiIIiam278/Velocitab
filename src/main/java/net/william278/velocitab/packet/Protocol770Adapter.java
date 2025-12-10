19b1474372ed648d95599f098361c520d8bcea0c
AlexDev_ <56083016+alexdev03@users.noreply.github.com>
2025-12-10 12:28:57 +0100

    Added support for Minecraft version 1.21.11 and updated plugin version to 1.7.10

--- src/main/java/net/william278/velocitab/packet/Protocol770Adapter.java
+++ src/main/java/net/william278/velocitab/packet/Protocol770Adapter.java
@@ -39,7 +39,8 @@ public class Protocol770Adapter extends Protocol765Adapter {
                 ProtocolVersion.MINECRAFT_1_21_5,
                 ProtocolVersion.MINECRAFT_1_21_6,
                 ProtocolVersion.MINECRAFT_1_21_7,
-                ProtocolVersion.MINECRAFT_1_21_9
+                ProtocolVersion.MINECRAFT_1_21_9,
+                ProtocolVersion.MINECRAFT_1_21_11
         ));
     }
 
